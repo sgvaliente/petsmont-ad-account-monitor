d28df78658735add04b99a8c256dadeb709e234d
Sebastian Valiente <sebastianvaliente@gmail.com>
2025-10-10 13:39:16 -0400

    Add simple test endpoint to debug deployment

---
+++ api/test.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  return res.status(200).json({ 
+    message: 'Test endpoint working!',
+    timestamp: new Date().toISOString(),
+    method: req.method,
+    url: req.url
+  });
+}
